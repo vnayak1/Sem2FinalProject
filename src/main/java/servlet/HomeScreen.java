74e841c2c8fa01bdfcccb990bb844536d2c8dedb
'pankaj' <'pankajtiwana@yahoo.com'>
2015-07-20 16:30:55 -0400

    "jax-rs and servlet working fine now"

--- src/main/java/servlet/HomeScreen.java
+++ src/main/java/servlet/HomeScreen.java
@@ -3,33 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import DatabaseCredentials.database;
-import com.google.gson.Gson;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import static java.util.Collections.list;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
- * @author vinayak
+ * @author c0652113
  */
-public class home extends HttpServlet {
+@WebServlet(name = "HomeScreen", urlPatterns = {"/HomeScreen"})
+public class HomeScreen extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -49,10 +39,10 @@ public class home extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");
+            out.println("<title>Servlet HomeScreen</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet HomeScreen at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         } finally {
@@ -72,47 +62,7 @@ public class home extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      //  processRequest(request, response);
-
-        PrintWriter out = response.getWriter();
-
-        //out.write("exception coccure123");
-        // JsonObjectBuilder json = Json.createObjectBuilder();
-        //JsonArrayBuilder productarray = Json.createArrayBuilder();
-        
-        user p = new user();
-        ArrayList<user> users= new ArrayList<user>();
-        
-        
-        Connection conn = database.getConnection();
-
-        if (conn == null) {
-            out.println("null");
-        } else {
-
-            out.println("not null");
-        }
-
-        try {
-            Statement smt = conn.createStatement();
-
-            ResultSet rs = smt.executeQuery("select * from master_data");
-
-            while (rs.next()) {
-
-                user  pnew = new user(rs.getInt(1),rs.getString(2),rs.getString(3), rs.getString(4),rs.getString(5));
-                users.add(pnew);
-                 
-            }
-            String json = new Gson().toJson(users);
-            out.print(json);
-
-        } catch (SQLException ex) {
-
-            out.write("exception coccure eallyy");
-            Logger.getLogger(home.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        processRequest(request, response);
     }
 
     /**
@@ -126,12 +76,7 @@ public class home extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       // processRequest(request, response);
-
-        PrintWriter out = response.getWriter();
-
-        out.println("Post method");
-
+        processRequest(request, response);
     }
 
     /**
